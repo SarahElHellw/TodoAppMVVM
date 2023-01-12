526b6aee85c5352392c0f38a7938f2d1ff35558f
SarahElHellw <sarah.mohamed.fatah@gmail.com>
2023-01-12 14:16:18 +0200

    initial commit

---
+++ app/src/main/java/com/sarahelhelw/projects/todoappmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sarahelhelw.projects.todoappmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
